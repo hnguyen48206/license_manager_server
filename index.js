d7a98d5f3adcce92779f29fb2b1404bb724b8b07
hnguyen48206 <hoangbich1989@gmail.com>
2024-04-15 09:25:13 +0700

    abc

--- index.js
+++ index.js
@@ -14,33 +14,11 @@ const Joi = require('joi')
     .extend(require('@joi/date'));
 
 const macs = require('os').networkInterfaces()
-console.log(macs)
+// console.log(macs)
 const APP_PORT = process.env.APP_PORT ? process.env.APP_PORT : '1010';
 var fs = require('fs');
 const jwt = require("jsonwebtoken");
-var privateKEY = fs.readFileSync('./assets/private.key', 'utf8');
 var publicKEY = fs.readFileSync('./assets/public.key', 'utf8');
-const sqlite3 = require("sqlite3").verbose();
-const filepath = "./data/lite.db";
-var database
-
-
-function createDbConnection() {
-    if (fs.existsSync(filepath)) {
-        database = new sqlite3.Database(filepath);
-    } else {
-        database = new sqlite3.Database(filepath, (error) => {
-            if (error) {
-                return console.error(error.message);
-            }
-        });
-        console.log("Connection with SQLite has been established");
-    }
-    database.exec('PRAGMA foreign_keys = ON;', pragmaErr => {
-        if (pragmaErr) console.log('Foreign key enforcement pragma query failed.')
-        else console.log('Foreign key enforcement pragma query ok.');
-    });
-}
 
 function verifyMacAddress(mac) {
     if (isObject(macs)) {
@@ -48,7 +26,7 @@ function verifyMacAddress(mac) {
         for (const [key, value] of Object.entries(macs)) {
             if (value.length > 0) {
                 value.forEach(interface => {
-                    if (interface.mac == mac)
+                    if (interface.mac.toLowerCase() == mac.toLowerCase())
                         check = true;
                 });
             }
@@ -64,232 +42,35 @@ function isObject(val) {
     return ((typeof val === 'function') || (typeof val === 'object'));
 }
 
-//////////////////////////////// Customer API //////////////////////////////////
-app.post('/create_customer', celebrate({
-    body: Joi.object({
-        customerName: Joi.string().min(1).max(200).required(),
-        note: Joi.string().min(0).max(500).required()
-    })
-}), function (req, res) {
-    var data = req.body;
-    runquery(`INSERT INTO Customers (CustomerName, Note)
-    VALUES (?, ?)`,
-        [data.customerName, data.note]).then(result => {
-            res.send({
-                code: 1,
-                message: 'Tạo mới _customer thành công',
-                data: result
-            });
-        }).catch(err => {
-            res.send({
-                code: 0,
-                message: 'Tạo mới _customer thất bại',
-                data: err
-            });
-        });
-});
-
-app.post('/update_customer', celebrate({
-    body: Joi.object({
-        customerID: Joi.number()
-            .integer()
-            .min(0).required(),
-        customerName: Joi.string().min(1).max(200).required(),
-        note: Joi.string().min(0).max(5000).required(),
-        status: Joi.number()
-            .integer()
-            .min(0)
-            .max(1).required(),
-    })
-}), function (req, res) {
-    var data = req.body;
-    runquery(
-        `UPDATE Customers SET CustomerName = ? , Status = ? ,  Note = ? WHERE CustomerID = ?`,
-        [data.customerName, data.status, data.note, data.customerID]
-    ).then(result => {
-        res.send({
-            code: 1,
-            message: 'Cập nhật _customer thành công',
-            data: result
-        });
-    }).catch(err => {
-        res.send({
-            code: 0,
-            message: 'Cập nhật _customer thất bại',
-            data: err
-        });
-    });
-});
-
-app.post('/delete_customer', celebrate({
-    body: Joi.object({
-        customerID: Joi.number()
-            .integer()
-            .min(0).required()
-    })
-}), function (req, res) {
-    var data = req.body;
-    runquery(`DELETE FROM Customers WHERE CustomerID = ?`, [data.customerID]).then(result => {
-        res.send({
-            code: 1,
-            message: 'Xóa _customer thành công', data: result
-        });
-    }).catch(err => {
-        res.send({
-            code: 0,
-            message: 'Xóa _customer thất bại', data: err
-        });
-    });
-});
-
-app.get('/get_all_customers', function (req, res) {
-    runSelectQuery(`SELECT * FROM Customers`).then(result => {
-        res.send({
-            code: 1,
-            message: 'Lấy danh sách _customer thành công', data: result
-        });
-    }).catch(err => {
-        res.send({
-            code: 0,
-            message: 'Lấy danh sách _customer thất bại', data: err
-        });
-    });
-});
-
 //////////////////////////////// License API //////////////////////////////////
-app.post('/create_license', celebrate({
-    body: Joi.object({
-        customerID: Joi.number()
-            .integer()
-            .min(0)
-    })
-}), function (req, res) {
-    var data = req.body;
-    runquery(`INSERT INTO Licenses (CustomerID)
-    VALUES (?)`,
-        [data.customerID]).then(result => {
-            res.send({
-                code: 1,
-                message: 'Tạo mới _license thành công', data: result
-            });
-        }).catch(err => {
-            res.send({
-                code: 0,
-                message: 'Tạo mới _license thất bại', data: err
-            });
-        });
-});
-
-app.post('/update_license', celebrate({
-    body: Joi.object({
-        status: Joi.number()
-            .integer()
-            .min(0)
-            .max(1).required(),
-        licenseID: Joi.number()
-            .integer()
-            .min(0).required(),
-        customerID: Joi.number()
-            .integer()
-            .min(0).required(),
-        licenseKey: Joi.string().min(10).max(5000).required()
-    })
-}), function (req, res) {
-    var data = req.body;
-    runquery(
-        `UPDATE Licenses SET CustomerID = ? , Status = ? ,  LicenseKey = ? WHERE LicenseID = ?`,
-        [data.customerID, data.status, data.licenseKey, data.licenseID]
-    ).then(result => {
-        res.send({
-            code: 1,
-            message: 'Cập nhật _license thành công', data: result
-        });
-    }).catch(err => {
-        res.send({
-            code: 0,
-            message: 'Cập nhật _license thất bại', data: err
-        });
-    });
-});
-
-app.post('/delete_license', celebrate({
-    body: Joi.object({
-        licenseID: Joi.number()
-            .integer()
-            .min(0).required()
-    })
-}), function (req, res) {
-    var data = req.body;
-    runquery(`DELETE FROM Licenses WHERE LicenseID = ?`, [data.licenseID]).then(result => {
-        res.send({
-            code: 1,
-            message: 'Xóa _license thành công', data: result
-        });
-    }).catch(err => {
-        res.send({
-            code: 0,
-            message: 'Xóa _license thất bại', data: err
-        });
-    });
-});
-
-app.get('/get_all_licenses', function (req, res) {
-    runSelectQuery(`SELECT * FROM Licenses`).then(result => {
-        res.send({
-            code: 1,
-            message: 'Lấy danh sách _license thành công', data: result
-        });
-    }).catch(err => {
-        res.send({
-            code: 0,
-            message: 'Lấy danh sách _license thất bại', data: err
-        });
-    });
-});
-
-app.post('/generate_license', celebrate({
-    body: Joi.object({
-        licenseDetail: Joi.object().required(),
-        expiresIn: Joi.string().min(2).max(5).required()
-    })
-}), function (req, res) {
-    var data = req.body;
-    console.log(data);
-    let generatedKey = '';
-    generatedKey = jwt.sign(
-        data.licenseDetail,
-        privateKEY,
-        {
-            algorithm: "RS256",
-            expiresIn: data.expiresIn ? data.expiresIn : '99y',
-        })
-    res.send({
-        code: 1,
-        message: 'Tạo licenseKey thành công',
-        generatedKey: generatedKey
-    }
-    );
-});
-
 app.post('/verify_license', celebrate({
     body: Joi.object({
         licenseKey: Joi.string().min(10).max(5000).required()
     })
 }), function (req, res) {
     let message = '';
+    let decoded_payload = null;
     jwt.verify(req.body.licenseKey, publicKEY, function (err, decoded) {
         if (err) {
-            console.log(err.name);
+            // console.log(err.name);
             if (err.name == 'TokenExpiredError')
                 message = 'expired'
             else
                 message = 'invalid'
         }
-        else
-            message = 'valid'
+        else {
+            let payload = jwt.decode(req.body.licenseKey);
+            if (payload['MAC'] != null && verifyMacAddress(payload['MAC'])) {
+                message = 'valid';
+                decoded_payload = payload;
+            }
+            else
+                message = 'wrong server indentification'
+        }
         res.send({
             code: 1,
-            message: message
+            message: message,
+            payload: decoded_payload
         })
     });
 });
@@ -305,56 +86,6 @@ process.on('exit', function (code) {
 });
 
 
-// Database Operations
-async function initDB() {
-    await runquery(`CREATE TABLE IF NOT EXISTS Customers
-    (
-      CustomerID INTEGER PRIMARY KEY AUTOINCREMENT,
-      CustomerName VARCHAR(200) NOT NULL UNIQUE,
-      Status INT NOT NULL DEFAULT 1,
-      Note TEXT DEFAULT NULL,
-      Created_at TEXT DEFAULT CURRENT_TIMESTAMP
-    );`);
-    await runquery(`CREATE TABLE IF NOT EXISTS Licenses
-    (
-      LicenseID INTEGER PRIMARY KEY AUTOINCREMENT,
-      CustomerID INTEGER NOT NULL,
-      Status INT NOT NULL DEFAULT 1,
-      LicenseKey TEXT DEFAULT NULL,
-      Created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID) ON UPDATE CASCADE ON DELETE CASCADE
-    );`);
-    return Promise.resolve(true);
-}
-function runquery(sql, params = []) {
-    // console.log(sql)
-    return new Promise((resolve, reject) => {
-        database.run(sql, params, function (err) {
-            if (err) {   //Trường hợp lỗi
-                console.log('Error running sql ' + sql)
-                console.log(err)
-                reject(err)
-            } else {   //Trường hợp chạy query thành công
-                resolve({ id: this.lastID })   //Trả về kết quả là một object có id lấy từ DB.
-            }
-        })
-    })
-}
-function runSelectQuery(sql, params = []) {
-    // console.log(sql)
-    return new Promise((resolve, reject) => {
-        database.all(sql, params, (error, row) => {
-            if (error) {
-                console.log('Error running sql ' + sql)
-                reject(err)
-            }
-            else
-                resolve(row);
-        })
-    })
-}
-
-
 // celebrate error handler, must be placed after all routes definition to works.
 app.use(errors());
 const server = require('http').Server(app);
@@ -365,8 +96,6 @@ server.listen(APP_PORT, "0.0.0.0", function (error) {
     }
     else {
         console.log('Server is listening on port', APP_PORT);
-        createDbConnection();
-        initDB();
     }
 })
 
